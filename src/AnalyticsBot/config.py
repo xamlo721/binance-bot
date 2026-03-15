483297ede79a53a7849598e7c9dc049ee1a2bc05
xamlo721 <thexamlomaximal@gmail.com>
2026-03-15 09:31:55 +0300

    update config & file perms

--- src/AnalyticsBot/config.py
+++ src/AnalyticsBot/config.py
@@ -1,10 +1,4 @@
 import os
 
 # Количество свечей, хранящихся в кеше
-MAX_CACHED_CANDLES: int = 1500  # 
-# Лимит Binance для klines endpoint
-MAX_CANDLES_PER_REQUEST: int = 1500
-# Binance API limit per minute = 1200 
-BINANCE_API_LIMIT: int = 800 
-# Количество пото
-THREAD_POOL_SIZE: int = 30 # 30
+MAX_CACHED_CANDLES: int = 2880  # 2880
--- src/DownloadBot/config.py
+++ src/DownloadBot/config.py
@@ -1,6 +1,6 @@
 
 # Количество свечей, хранящихся в кеше
-MAX_CACHED_CANDLES: int = 2880  # 
+MAX_CACHED_CANDLES: int = 2880 # 2880
 # Лимит Binance для klines endpoint
 MAX_CANDLES_PER_REQUEST: int = 1500
 # Binance API limit requests per minute = 1200 
@@ -8,7 +8,7 @@ BINANCE_API_REQUEST_LIMIT: int = 800
 # Binance API limit wight per minute = 2400
 BINANCE_API_WEIGHT_LIMIT: int = 2000
 # Количество потоков, участвующих в запросе сервера
-THREAD_POOL_SIZE: int = 32 # 30
+THREAD_POOL_SIZE: int = 8 # 30
 # UDP IP, PORT
 DOWNLOADER_UDP_IP: str = "127.0.0.1"
 DOWNLOADER_UDP_PORT: int = 58001

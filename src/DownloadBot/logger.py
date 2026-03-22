cf024d69a51e2523f52b96c7fe748b4659722153
xamlo721 <thexamlomaximal@gmail.com>
2026-03-22 11:54:37 +0300

    Update loggers

--- src/DownloadBot/logger.py
+++ src/DownloadBot/logger.py
@@ -1,8 +1,14 @@
 import logging
 
+# Создаём логгер
 logger = logging.getLogger("download_bot")
-handler = logging.StreamHandler()
-formatter = logging.Formatter('[%(asctime)s][%(levelname)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-logger.setLevel(logging.INFO)
+
+# Добавляем обработчик только если его ещё нет
+if not logger.handlers:
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter('[%(asctime)s][%(levelname)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+    # Предотвращаем дублирование через корневой логгер (опционально)
+    logger.propagate = False
--- src/TraderBot/logger.py
+++ src/TraderBot/logger.py
@@ -1,8 +1,13 @@
 import logging
 
+# Создаём логгер
 logger = logging.getLogger("trader_bot")
-handler = logging.StreamHandler()
-formatter = logging.Formatter('[%(asctime)s][%(levelname)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-logger.setLevel(logging.INFO)
+
+# Если у логгера ещё нет обработчиков, добавляем их
+if not logger.handlers:
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter('[%(asctime)s][%(levelname)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+    logger.propagate = False  # предотвращает передачу сообщений корневому логгеру, если не нужно
